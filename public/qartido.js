3e5d56427a9457958c9d6aa3f1484f5f8e06cc08
Kevin Sun <kevsun1@gmail.com>
2016-09-19 12:57:29 -0400

    yellow -> gold for EU, to keep borders visible

--- public/qartido.js
+++ public/qartido.js
@@ -80,7 +80,7 @@ d3.json("europe.json", function(error, europe) {
         .on("click", click);
 
     coloringOnClick("#upcoming-color", function (d) { return upcomingCountries.indexOf(d.id) >= 0 ? '#0F7173' : '#DB8'; })
-    coloringOnClick("#eu-color", function (d) { return parlgov[d.id] ? parlgov[d.id].eu_accession_date ? 'yellow' : '#DB8' : '#DB8'; })
+    coloringOnClick("#eu-color", function (d) { return parlgov[d.id] ? parlgov[d.id].eu_accession_date ? 'gold' : '#DB8' : '#DB8'; })
 
     d3.selectAll(".active").classed("active", false);
 
