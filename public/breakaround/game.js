1b7705240093cfc526c74f434f65a41fe2a950a1
Kevin Sun <kevsun1@gmail.com>
2017-08-10 16:11:16 -0400

    copy data from campaign repo

--- public/breakaround/game.js
+++ public/breakaround/game.js
@@ -1,8 +1,3 @@
-var BreakAround = {}
-
-var BALL_VELOCITY = 200;
-var BALL_SIZE = 23;
-
 BreakAround.Game = function () {
     this.paddle;
     this.ball;
@@ -12,9 +7,21 @@ BreakAround.Game = function () {
     this.graphics;
 
     this.score;
+    this.highScore;
+    this.bounces;
+    this.leastBounces;
     this.scoreText;
+    this.bounceText;
 
     this.alreadyIntersected;
+
+    this.pointSound;
+    this.bounceSound;
+    this.deathSound;
+    this.levelupSound;
+
+    this.showTrajectory;
+
 };
 
 BreakAround.Game.prototype = {
@@ -31,13 +38,7 @@ BreakAround.Game.prototype = {
     },
 
     preload: function () {
-
-        this.load.path = 'assets/';
-
         alreadyIntersected = false;
-
-        this.load.images(['paddle', 'ball', 'hotdog', 'emoji', 'background']);
-
     },
 
     create: function () {
@@ -54,29 +55,33 @@ BreakAround.Game.prototype = {
         ball.body.setCircle(BALL_SIZE);
 
         emojis = this.add.group();
-        for (var i = 0; i < 20; i++) {
-            var p = new Phaser.Point();
-            var c = new Phaser.Circle(radius, radius, 450);
-            c.random(p);
-            var emoji = emojis.create(p.x, p.y, 'emoji');
-            emoji.anchor.setTo(0.5, 0.5);
-            this.game.physics.enable(emoji, Phaser.Physics.ARCADE);
-            emoji.body.setCircle(25);
-        }
+        this.respawnEmoji();
 
-        var theta = this.rnd.realInRange(-Math.PI, Math.PI);
-        ball.body.velocity.x = BALL_VELOCITY * Math.cos(theta);
-        ball.body.velocity.y = BALL_VELOCITY * Math.sin(theta);
+        showTrajectory = false;
+        var tKey = game.input.keyboard.addKey(Phaser.Keyboard.T);
+        tKey.onDown.add(function () {
+            showTrajectory = !showTrajectory;
+        }, this);
+
+        var textStyle = {
+            'fill': 'white',
+            'font': '10pt Courier New'
+        };
 
         score = 0;
-        scoreText = this.add.text(10, 10, 'Score: 0', {
-            'fill': 'white',
-            'font': '20pt Courier New'
-        });
+        highScore = 0;
+        bounces = 0;
+        leastBounces = null;
+        scoreText = this.add.text(10, 10, 'Score: 0\nHigh Score: 0', textStyle);
+        bounceText = this.add.text(10, 590, 'Bounces: 0\nLeast Bounces/20: N/A', textStyle);
+        bounceText.anchor.setTo(0, 1);
 
         background.scale.x = 0.8;
         background.scale.y = 0.8;
-        var tween = this.game.add.tween(background.scale).to({x: 1.1, y: 1.1}, 2000, "Linear", true, 0, -1);
+        var tween = this.game.add.tween(background.scale).to({
+            x: 1.1,
+            y: 1.1
+        }, 2000, "Linear", true, 0, -1);
         tween.yoyo(true, 0);
 
         this.game.input.x = radius;
@@ -84,10 +89,19 @@ BreakAround.Game.prototype = {
 
         graphics = game.add.graphics(0, 0);
 
+        pointSound = this.game.add.audio('point');
+        bounceSound = this.game.add.audio('bounce');
+        deathSound = this.game.add.audio('death');
+        levelupSound = this.game.add.audio('levelup');
+
+        var theta = this.rnd.realInRange(-Math.PI, Math.PI);
+        ball.body.velocity.x = BALL_VELOCITY * Math.cos(theta);
+        ball.body.velocity.y = BALL_VELOCITY * Math.sin(theta);
+
     },
 
     update: function () {
-        
+
         ball.rotation += Math.PI / 10;
 
         var angleFromCenter = this.game.math.angleBetween(radius, radius,
@@ -118,6 +132,11 @@ BreakAround.Game.prototype = {
                 ball.body.velocity.x = BALL_VELOCITY * Math.cos(reflectAngle);
                 ball.body.velocity.y = BALL_VELOCITY * Math.sin(reflectAngle);
 
+                bounceSound.play();
+                bounces++;
+                bounceText.setText('Bounces: ' + bounces + '\nLeast Bounces/20: ' +
+                    (leastBounces === null ? 'N/A' : leastBounces));
+
             }
             alreadyIntersected = true;
         } else {
@@ -128,29 +147,43 @@ BreakAround.Game.prototype = {
             ball.x = radius;
             ball.y = radius;
 
+            this.respawnEmoji();
+
             var theta = this.rnd.realInRange(-Math.PI, Math.PI);
             ball.body.velocity.x = BALL_VELOCITY * Math.cos(theta);
             ball.body.velocity.y = BALL_VELOCITY * Math.sin(theta);
+            deathSound.play();
 
+            score = 0;
+            scoreText.setText('Score: ' + score + '\nHigh Score: ' + highScore);
+            bounces = 0;
+            bounceText.setText('Bounces: ' + bounces + '\nLeast Bounces/20: ' +
+                (leastBounces === null ? 'N/A' : leastBounces));
         }
 
         this.game.physics.arcade.overlap(emojis, ball, function (e, b) {
             b.kill();
             score++;
+            pointSound.play();
         }, null, this);
 
-        scoreText.setText('Score: ' + score);
+        if (score > highScore) {
+            highScore = score;
+        }
+
+        scoreText.setText('Score: ' + score + '\nHigh Score: ' + highScore);
+
 
         if (emojis.countLiving() === 0) {
-            for (var i = 0; i < 20; i++) {
-                var p = new Phaser.Point();
-                var c = new Phaser.Circle(radius, radius, 450);
-                c.random(p);
-                var emoji = emojis.create(p.x, p.y, 'emoji');
-                emoji.anchor.setTo(0.5, 0.5);
-                this.game.physics.enable(emoji, Phaser.Physics.ARCADE);
-                emoji.body.setCircle(25);
+            if (bounces < leastBounces || leastBounces === null) {
+                leastBounces = bounces;
             }
+            levelupSound.play();
+            this.respawnEmoji();
+            bounces = 0;
+            bounceText.setText('Bounces: ' + bounces + '\nLeast Bounces/20: ' +
+                leastBounces);
+
         }
     },
 
@@ -162,8 +195,9 @@ BreakAround.Game.prototype = {
         graphics.moveTo(radius, radius);
         graphics.lineTo(paddle.x, paddle.y);
 
-        /*var paddleCos = Math.cos(paddle.rotation) * 95;
-        var paddleSin = Math.sin(paddle.rotation) * 95;
+
+
+        /*
         graphics.lineStyle(10, 0xffff00, 1);
         graphics.moveTo(paddle.x - paddleCos, paddle.y - paddleSin);
         graphics.lineTo(paddle.x + paddleCos, paddle.y + paddleSin);
@@ -175,26 +209,47 @@ BreakAround.Game.prototype = {
         graphics.moveTo(ball.x - ballCos, ball.y - ballSin);
         graphics.lineTo(ball.x + ballCos, ball.y + ballSin);
 
-
-        var ballMovementLine = new Phaser.Line(0, 0, ball.body.velocity.x, ball.body.velocity.y);
-        var paddleLine = new Phaser.Line(paddle.x - paddleCos, paddle.y - paddleSin, paddle.x + paddleCos, paddle.y + paddleSin);
-
-        var reflect = ballMovementLine.reflect(paddleLine);
-
-        graphics.lineStyle(3, 0xff0000, 1);
-        graphics.moveTo(paddle.x, paddle.y);
-        graphics.lineTo(paddle.x + Math.cos(reflect) * 200, paddle.y + Math.sin(reflect) * 200);
-
         this.game.debug.body(paddle);
         this.game.debug.body(ball);*/
 
+        if (showTrajectory) {
+            var paddleCos = Math.cos(paddle.rotation) * 95;
+            var paddleSin = Math.sin(paddle.rotation) * 95;
+            var ballMovementLine = new Phaser.Line(ball.x, ball.y, ball.x + ball.body.velocity.x * 900, ball.y + ball.body.velocity.y * 900);
+            var paddleLine = new Phaser.Line(paddle.x - paddleCos, paddle.y - paddleSin, paddle.x + paddleCos, paddle.y + paddleSin);
+
+            var intersect = ballMovementLine.intersects(paddleLine, true);
+            var reflect = ballMovementLine.reflect(paddleLine);
+
+            graphics.lineStyle(1, 0xffff00, 0.5);
+            if (intersect) {
+                /* adjust because intersect point != turning point */
+                intersect.x -= BALL_SIZE * ball.body.velocity.x / BALL_VELOCITY;
+                intersect.y -= BALL_SIZE * ball.body.velocity.y / BALL_VELOCITY;
+
+                graphics.moveTo(ball.x, ball.y);
+                graphics.lineTo(intersect.x, intersect.y);
+                graphics.lineTo(intersect.x + Math.cos(reflect) * 900, intersect.y + Math.sin(reflect) * 900);
+            } else {
+                graphics.moveTo(ball.x, ball.y);
+                graphics.lineTo(ball.x + ball.body.velocity.x * 4.5, ball.y + ball.body.velocity.y * 4.5);
+            }
+        }
+
+    },
+
+    respawnEmoji: function () {
+        emojis.removeAll();
+
+        for (var i = 0; i < 20; i++) {
+            var p = new Phaser.Point();
+            var c = new Phaser.Circle(radius, radius, 450);
+            c.random(p);
+            var emoji = emojis.create(p.x, p.y, 'emoji');
+            emoji.anchor.setTo(0.5, 0.5);
+            this.game.physics.enable(emoji, Phaser.Physics.ARCADE);
+            emoji.body.setCircle(25);
+        }
     }
 
-};
-
-
-var game = new Phaser.Game(600, 600, Phaser.AUTO, 'game');
-
-game.state.add('BreakAround.Game', BreakAround.Game);
-
-game.state.start('BreakAround.Game');
+}
